03c6adc586af8d5a51a0e239fafca69269ebfc3a
Scott Young <young.scottw@gmail.com>
2017-08-18 12:46:08 -0500

    Build FlightXML get_wx_obs for retrieving single wx observations

---
+++ flightxml.py
@@ -0,0 +1,22 @@
+import requests
+import apisecrets as API
+
+KEY = API.API_PASSWORD
+USERNAME = API.API_USERNAME
+URI = API.API_URI
+
+
+def get_airport_wx(airport_code):
+    payload = {
+        'airport_code': airport_code,
+        'howMany': '1',
+        'offset': '0'
+    }
+    print(payload)
+    response = requests.get(URI + "WeatherConditions", params=payload, auth=(USERNAME, KEY))
+    print(response.text)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return "There was an error retrieving the data from the server. {}".format(response.status_code)
