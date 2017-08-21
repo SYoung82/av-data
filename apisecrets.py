a319a003820d0253bd909bf1148c473116cf97d1
Scott Young <young.scottw@gmail.com>
2017-08-21 11:03:11 -0500

    Ignore api file

--- apisecrets.py
+++
@@ -1,4 +0,0 @@
-GUEST_PASSWORD = '3D8PUpp^Vq4eow!'
-API_PASSWORD = 'be5ef3606d2fddf3b3f098c6b3ff83bf2e87045c'
-API_USERNAME = 'youngscottw'
-API_URI = 'https://flightxml.flightaware.com/json/FlightXML3/'
